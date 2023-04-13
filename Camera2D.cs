f7d26ffe4b917ef5d09b1b04eccdbcd948555157
timothek2112 <nikzav6080@gmail.com>
2023-04-14 00:42:54 +0700

    Created base camera and rotation (needs to be fixed)

---
+++ Camera2D.cs
@@ -0,0 +1,67 @@
+﻿using OpenTK.Mathematics;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
+namespace Tutorial
+{
+    public class Camera2D
+    {
+        public Vector3 Position;
+        public Vector3 Rotation;
+        public Vector3 Scale = Vector3.One;
+        public float zoomFactor = 0.01f;
+        public float moveSpeed = 0.2f;
+
+        public float Size = 0.05f;
+
+        public void Update(float deltaTime, KeyboardState keysState, MouseState mouseState)
+        {
+            if (keysState.IsKeyDown(Keys.W))
+            {
+                Position -= Vector3.UnitY * moveSpeed * deltaTime;
+            }
+            if (keysState.IsKeyDown(Keys.S))
+            {
+                Position += Vector3.UnitY * moveSpeed * deltaTime;
+            }
+            if (keysState.IsKeyDown(Keys.A))
+            {
+                Position -= Vector3.UnitX * moveSpeed * deltaTime;
+            }
+            if (keysState.IsKeyDown(Keys.D))
+            {
+                Position += Vector3.UnitX * moveSpeed * deltaTime;
+            }
+            if(mouseState.IsButtonDown(MouseButton.Right)) 
+            {
+                Rotation += new Vector3(mouseState.Delta.Y, mouseState.Delta.X, 0) * deltaTime;
+            }
+            Console.WriteLine(Rotation);
+            moveSpeed = Size * 2;
+            Size -= mouseState.ScrollDelta.Y * zoomFactor;
+            Size = Math.Clamp(Size, 0, 10);
+        }
+
+
+        public void Render(int width, int height)
+        {
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadIdentity();
+
+            Matrix4 proj =
+                       Matrix4.CreateTranslation(new Vector3(-Position.X * width, -Position.Y * height, -Position.Z)) *
+                        Matrix4.CreateScale(Scale.X, -Scale.Y, Scale.Z) * Matrix4.CreateRotationX(Rotation.X) * Matrix4.CreateRotationY(Rotation.Y) * Matrix4.CreateRotationZ(Rotation.Z) *
+                      Matrix4.CreateOrthographic(width * Size, height * Size, -100, 1000)
+                   ;
+            GL.LoadMatrix(ref proj);
+
+            GL.MatrixMode(MatrixMode.Modelview);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,87 @@
+﻿using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Mathematics;
+
+namespace Tutorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            InitGame();
+        }
+
+        private static void InitGame()
+        {
+            GameWindowSettings windowSettings = new GameWindowSettings() { };
+            NativeWindowSettings nativeSettings = new NativeWindowSettings() {
+                StartFocused = true,
+                StartVisible = true,
+                Vsync = VSyncMode.On,
+                APIVersion = new Version(3, 3),
+                Profile = ContextProfile.Compatability,
+                Flags = ContextFlags.Default
+            };
+            using(Game game = new Game(windowSettings, nativeSettings))
+            {
+                game.Run();
+            }
+        }
+    }
+
+    class Game : GameWindow
+    {
+        private Camera2D mainCamera = new Camera2D();
+        private int indDisplayList = 0;
+        public Game(GameWindowSettings gameWindowSettings, NativeWindowSettings nativeWindowSettings) : base(gameWindowSettings, nativeWindowSettings)
+        {
+        }
+
+        protected override void OnLoad()
+        {
+            GL.PolygonMode(MaterialFace.Front, PolygonMode.Fill);
+            GL.PolygonMode(MaterialFace.Back, PolygonMode.Line);
+            GL.ClearColor(0.5f, 0, 0.5f, 1);
+
+            
+            base.OnLoad();
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs args)
+        {
+            
+            base.OnUpdateFrame(args);
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs args)
+        {
+            GL.ClearColor(0.5f, 0.5f, 0f, 1f);
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+
+            GL.Begin(PrimitiveType.TriangleStrip);
+            GL.Vertex3(0, 0, 0);
+            GL.Vertex3(0, 1, 0);
+            GL.Vertex3(1, 0, 0);
+            GL.End();
+
+            mainCamera.Update((float)args.Time, KeyboardState, MouseState);
+            mainCamera.Render(800,600);
+
+            SwapBuffers();
+            base.OnRenderFrame(args);
+        }
+
+        private int CreateVBO(float[] data)
+        {
+            int vbo = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ArrayBuffer, vbo);
+            GL.BufferData(BufferTarget.ArrayBuffer, data.Length * sizeof(float), data, BufferUsageHint.StaticDraw);
+            GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+            return vbo;
+        }
+
+        
+    }
+}
